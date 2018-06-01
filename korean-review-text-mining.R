deb18da965d9e46ecf7845242a1deb6bc4fe39c4
Jihye Lee <psycho5cafe@gmail.com>
2018-06-01 18:26:37 +0900

    Update korean-review-text-mining.R

--- korean-review-text-mining.R
+++ korean-review-text-mining.R
@@ -23,17 +23,35 @@ library(tm)
 library(stringr)
 library(wordcloud2)
 
+#library(KoNLP)와 연계해서 사용하는 useNIADic 
 useNIADic(which_dic = c("woorimalsam", "insighter"), backup = T)
+#튜토리얼 참고; https://kbig.kr/portal/kbig/knowledge/files/bigdata_report.page?bltnNo=10000000016451
 
-
-#
-#read.lines; text file should be saved with utf-8 encoding 
+#read.lines; text file should be saved with utf-8 encoding (not ASNI)
 data <- readLines("Androidreview_20180529.txt", encoding="UTF-8")
 
-#특수문자, 기호 전처리 
+####특수문자, 기호 전처리 
 data <- str_replace_all(data,"[^[:graph:]]", " ")
 #data <- str_replace_all(data,"[^[ㄱ-흫]]", " ") #어미를 날리는 용도인듯
 data <- data[which(data != "")]
+
+####명사 추출해서 데이터 상황 알아보기 
+extractNoun(data[1])
+extractNoun(data[2])
+extractNoun(data[3])
+
+#read.lines; text file should be saved with utf-8 encoding 
+data <- readLines("Androidreview_20180529.txt", encoding="UTF-8")
+
+####특수문자, 기호 전처리 
+data <- str_replace_all(data,"[^[:graph:]]", " ")
+data <- str_replace_all(data,"[^[ㄱ-흫]]", " ") #어미를 날리는 용도인듯
+data <- data[which(data != "")]]
+
+####명사 추출해서 데이터 상황 알아보기 
+extractNoun(data[1])
+extractNoun(data[2])
+extractNoun(data[3])
 
 #data_corpus <- sapply(data, function(x) {paste(extractNoun(x), collapse=" ")}, USE.NAMES=F)
 
@@ -45,7 +63,8 @@ data_corpus <- tm_map(data_corpus, removeNumbers)
 data_corpus <- tm_map(data_corpus, tolower) #한국어에는 lower 없음 
 data_corpus_test <- tm_map(data_corpus, stripWhitespace)
 
-#data_stopwords <- c(stopwords("korean"), "되", "하면", "것", "그것", "음", "수", "들이", "한", "할", "있", "년", "하기", "하지", "하")
+data_stopwords <- c(stopwords("english"), "되", "하면", "것", "그것", "음", "수", "들이", "한", "할", "있", "년", "하기", "하지", "하")
+#stopwords는 english로 적혀있는 경우에만 작동함 
 
 #데이터 정제 경과 확인 
 inspect(data_corpus[1])
@@ -57,10 +76,19 @@ class(docs)
 data_corpus_test <- tm_map(data_corpus, removeWords, data_stopwords)
 data_corpus_test <-tm_map(data_corpus, PlainTextDocument)
 
-#tdm utf-8 깨짐 방지 필요 
+#tdm utf-8 깨짐 방지를 위해서는 일단 메모장에서 utf-8로 텍스트를 저장할 것 
 data_tdm <- TermDocumentMatrix(data_corpus_test, control=list(wordLengths=c(2,Inf)))
-Encoding(data_tdm$dimnames$Terms) = 'UTF-8'
-as.matrix(data_tdm)
+
+#data_tdm이 오류가 나는 경우, TDM이 인자로 Corpus만 받기 때문임을 기억하고 corpus 변환 후 tdm으로 재변환 요망
+#ref: http://lightblog.tistory.com/52 
+doc <- Corpus(VectorSource(data_corpus_test))
+tdm <- TermDocumentMatrix(doc)
+#Encoding(data_tdm$dimnames$Terms) = 'UTF-8'
+tdm2 <- as.matrix(tdm)
+tdm3 <- rowSums(tdm2)
+tdm4 <- tdm3[order(tdm3, decreasing = T)]
+tdm4
+
 
 ########### 
 
